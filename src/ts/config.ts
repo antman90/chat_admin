6e726988203fe32fa90093cc711ffb910314ffa0
mf <925697@163>
2023-01-04 14:30:40 +0800

    update

--- src/ts/config.ts
+++ src/ts/config.ts
@@ -1,6 +1,8 @@
-export const apiAddress = "http://boardapi.11toon.link/api/";
+// export const apiAddress = "http://boardapi.11toon.link/api/";
+export const apiAddress = "http://chat-api.site/api/";
 export const apiSiteName = "CHAT_ADMIN";
 export const siteName = "CHAT ADMIN";
 export const pageSize = 3;
-export const chatWsLink = "ws://chatting01.fusoft001.com";
+// export const chatWsLink = "ws://chatting01.fusoft001.com";
+export const chatWsLink = "ws://192.168.3.44:9005";
 export const userListRefreshTime = 10 * 1000;
